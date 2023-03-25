ef1d8fd8ecb55388986b3c39c501249708642b20
sandeep <php.dbvertex@gmail.com>
2023-03-25 17:56:49 +0530

    h1111

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'https://dbvertex.com/Electricalvehical/';
-///test
+///test111111111111
 /*
 |--------------------------------------------------------------------------
 | Index File
